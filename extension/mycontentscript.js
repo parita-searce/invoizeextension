d421a17d991b1bf023d02c5455cc6da0030164e9
Parita Khilosiya <parita.khilosiya@searce.com>
2015-08-20 12:29:33 +0530

    Changes in file paths

--- extension/mycontentscript.js
+++ extension/mycontentscript.js
@@ -1,27 +1,8 @@
 
 var newdiv = document.createElement("div");
-newdiv.innerHTML='<object type="text/html" data="http://localhost/extension/index.html" ></object>';
+newdiv.innerHTML='<object style="height:100%" type="text/html" data="http://localhost/ext/extension/index.html" ></object>';
 newdiv.setAttribute("style", "border:none; width:300px; display:block; position:absolute; top:0; right:0px; height:100%");
 document.body.appendChild(newdiv);
-
-
-/*
-var iframe = document.createElement("iframe");
-iframe.setAttribute("src", "http://localhost/extension/index.html");
-iframe.setAttribute("style", "border:none; width:300px; display:block; position:absolute; top:0; right:0px; height:100%");
-iframe.setAttribute("scrolling", "no");
-iframe.setAttribute("frameborder", "0");
-document.body.appendChild(iframe);
-*/
-
-
-
-
-/*
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response);
-});*/
-
 
 document.addEventListener("hello", function(data) {
     chrome.runtime.sendMessage("test"+data, 
@@ -57,13 +38,29 @@ var myExtension =
     
   }
 }
-
 document.addEventListener("MyExtensionEvent", function(e) { myExtension.myListener(e); }, false, true);
 
+
+
+function receiveMessage(event)
+{	
+	
+	AnswerEvt = document.createElement("MyExtensionAnswer");
+	AnswerEvt.setAttribute("Part1", "answers this.");
+
+	document.documentElement.appendChild(AnswerEvt);
+
+	var event = document.createEvent("HTMLEvents");
+	event.initEvent("MyAnswerEvent", true, false);
+	AnswerEvt.dispatchEvent(event);
+
+}
+document.addEventListener("fromExtension", receiveMessage, false, true);
+
+
+
 /*
-
 var myPort = chrome.extension.connect();
-
 myPort.onMessage.addListener(function(data) {
     console.log( 'got a message' );
     if( data.message == 'doaction' )
@@ -78,20 +75,4 @@ myPort.onMessage.addListener(function(data) {
 		AnswerEvt.dispatchEvent(event);        
     } // end if
 });
-
 */
-
-document.addEventListener("fromExtension", receiveMessage, false, true);
-function receiveMessage(event)
-{	
-	
-	AnswerEvt = document.createElement("MyExtensionAnswer");
-	AnswerEvt.setAttribute("Part1", "answers this.");
-
-	document.documentElement.appendChild(AnswerEvt);
-
-	var event = document.createEvent("HTMLEvents");
-	event.initEvent("MyAnswerEvent", true, false);
-	AnswerEvt.dispatchEvent(event);
-
-}
